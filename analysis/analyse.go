0fa1d528207cd9099e28fbb1822362f2c8b3d9b4
Eric Myhre <hash@exultant.us>
2021-08-15 14:57:14 +0200

    hello analysis continues

--- analysis/analyse.go
+++ analysis/analyse.go
@@ -1,8 +1,12 @@
 package analysis
 
 import (
+	"fmt"
+	"go/ast"
+
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 var Analyzer = &analysis.Analyzer{
@@ -13,5 +17,23 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	nodeFilter := []ast.Node{
+		(*ast.File)(nil),
+		(*ast.CallExpr)(nil),
+	}
+
+	inspect.Preorder(nodeFilter, func(node ast.Node) {
+		switch stmt := node.(type) {
+		case *ast.File:
+			fmt.Printf("%#v\n", stmt)
+		case *ast.CallExpr:
+			fmt.Printf("%#v\n", stmt)
+			pass.Reportf(stmt.Fun.Pos(), "a call")
+			fmt.Printf("%s\n", pass.Fset.PositionFor(stmt.Fun.Pos(), true))
+		}
+	})
+
 	return nil, nil
 }
--- analysis/testdata/main.go
+++ analysis/testdata/main.go
@@ -1,5 +1,20 @@
 package main
 
 func main() {
+	One()
+	Two()
+	err := Three()
+	_ = err
+}
 
+func One() {
+
+}
+
+func Two() {
+	One()
+}
+
+func Three() error {
+	return nil
 }
