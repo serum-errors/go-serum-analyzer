c7c2c04e031b08fafd86f6590396a11ac7e97722
Eric Myhre <hash@exultant.us>
2021-08-15 16:17:07 +0200

    hello analysis: starting to write some print helpers to get a reasonable volume of quick info as I frolic about.
    I'm sort of disappointed in what the stdlib has for this.
    (Either that or I'm just not finding the right incantations yet.)
    The printer and formatter stuff is of course advanced, but...
    has interesting restrictions, such as only working starting from
    a certain list of top-level node types.  While I'm sure that's
    very useful for the tools that those things are used to build,
    it's not very useful to me right now in debugging and playing.

--- analysis/analyse.go
+++ analysis/analyse.go
@@ -3,6 +3,8 @@ package analysis
 import (
 	"fmt"
 	"go/ast"
+	"reflect"
+	"strings"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -34,18 +36,51 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		case *ast.FuncDecl: // n.b. does not include inlines -- that's a "FuncLit".  *does* include methods, though.
 			fmt.Printf("\tfunc name: %#v\n", stmt.Name.Name)
 			if stmt.Recv != nil { // not really sure why there's a list here, it's either nil or it's one element.
-				for idx, field := range stmt.Recv.List {
-					fmt.Printf("\trecv[%d]: %s : %s\n",
-						idx,
-						field.Names, // careful: you're experiencing AST here: it can have multiple names per type.  I wonder if I want a higher level view for this.
-						field.Type,
-					)
-				}
+				fmt.Printf("\trecv: %s\n", quickString(stmt.Recv))
 			}
-			fmt.Printf("\tfunc sig: %#v\n", stmt.Type)
+			fmt.Printf("\tfunc sig: %s -> %s\n", quickString(stmt.Type.Params), quickString(stmt.Type.Results))
 			fmt.Printf("\tbody: %#v\n", stmt.Body.List)
 		}
 	})
 
 	return nil, nil
+}
+
+// quickString spits out a string for something quickly and briefly, and not entirely recursively.
+//
+// you can use `ast.Fprint(os.Stdout, pass.Fset, stmt, ast.NotNilFilter)` if you're really wanting a mouthful,
+// but it's a truly huge output, includes lots of position info you'd probably find redundant, etc.
+func quickString(x interface{}) string {
+	if reflect.ValueOf(x).IsNil() {
+		return fmt.Sprintf("%T(nil)", x)
+	}
+	switch y := x.(type) {
+	case *ast.FieldList:
+		var sb strings.Builder
+		sb.WriteString("(")
+		for _, field := range y.List {
+			for _, name := range field.Names { // mind: you're experiencing AST here: it can have multiple names per type.
+				sb.WriteString(name.Name)
+				sb.WriteString(" ")
+				sb.WriteString(quickString(field.Type)) // this can be a whole heckin expression, technically.  (and often is, because commonly it has at least a "StarExpr" kicking it off.)
+				sb.WriteString(",")
+			}
+			if len(field.Names) == 0 {
+				// or, it can have no name at all, just a type.
+				sb.WriteString("_ ")
+				sb.WriteString(quickString(field.Type))
+				sb.WriteString(",")
+			}
+		}
+		sb.WriteString(")")
+		return sb.String()
+	case *ast.StarExpr:
+		return fmt.Sprintf("*(%v)", y.X)
+	case fmt.Stringer:
+		return y.String()
+	case fmt.GoStringer:
+		return y.GoString()
+	default:
+		return fmt.Sprintf("%#v", x)
+	}
 }
