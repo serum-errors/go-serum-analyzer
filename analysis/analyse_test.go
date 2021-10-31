58750754820a634964720f47c8ee354066b4bdbd
Janis Peyer <janis.peyer@outlook.com>
2021-10-31 20:10:44 +0100

    Removed old toy code

--- analysis/analyse_test.go
+++ analysis/analyse_test.go
@@ -6,11 +6,6 @@ import (
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
-func TestToyAnalyzer(t *testing.T) {
-	t.Skip("this was for experiments only, not really a test")
-	analysistest.Run(t, analysistest.TestData(), ToyAnalyzer, "toy")
-}
-
 func TestVerifyAnalyzer(t *testing.T) {
 	analysistest.Run(t, analysistest.TestData(), VerifyAnalyzer, "001")
 	analysistest.Run(t, analysistest.TestData(), VerifyAnalyzer, "docformat")
